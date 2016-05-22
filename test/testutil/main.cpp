157955b62e709981b7e6f824080ece6a0bba4995
DayBreakZhang <a365316465@126.com>
2016-05-22 21:56:43 +0800

    add
    add

--- test/testutil/main.cpp
+++ test/testutil/main.cpp
@@ -6,7 +6,14 @@
 using namespace std;
 using namespace gpl;
 
+void teststringtoini(std::string d)
+{
+	util ui;
+	int a = ui.StringToInt(d.c_str());
+	printf("%d", a);
+}
 int main(int argc, char *argv[])
 {
+	teststringtoini("233");
 	return 0;
 }
