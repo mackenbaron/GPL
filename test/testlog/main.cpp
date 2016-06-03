3024adb2c8f1dbaae3e48df66ff0fc1afcc62fe9
DayBreakZhang <a365316465@126.com>
2016-06-03 18:19:03 +0800

    add
    add

--- test/testlog/main.cpp
+++ test/testlog/main.cpp
@@ -7,12 +7,12 @@ int main(int argc, char *argv[])
 	LOGINIT();
  	LOGINFO("First Number: %d.", 1);
  	LOGERROR("Second Number: %d.", 2);
+	LOGTINFO("Second Number: %d.", 2);
  	std::string src = "ÖÐ¹ú";
  	LOGINFO("First Number: %s.", src.c_str());
 	LOGINFO("First Number: %d.", 1);
 	LOGERROR("Second Number: %d.", 2);
 	LOGINFO("First Number: %s.", src.c_str());
-	LOGTINFO("First Number: %d.", 1);
 	system("pause");
 	return 1;
 }
