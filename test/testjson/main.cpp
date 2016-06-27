846b9ea9ee17a45d0bfbbdae8490e368141dff7f
DayBreakZhang <a365316465@126.com>
2016-06-27 21:11:02 +0800

    update
    在使用xpath的时候使用数组，前不应有/

--- test/testjson/main.cpp
+++ test/testjson/main.cpp
@@ -6,9 +6,9 @@ int main(int argc, char *argv[])
 {
 	json js;
 
-	bool s = js.parseJson("e:\\a.json");
-	int size = js.getArraySize("/data/children");
+	bool s = js.parseJson("e:\\test.json");
+	//int size = js.getArraySize("/data/children");
 	std::string url = "";
-	js.getItemDate(url, "/data/children/[1]/data/thumbnail");
+	js.getItemDate(url, "/data/children[1]/data/thumbnail");
 	std::cout << "url" << url.c_str() << std::endl;
 }
